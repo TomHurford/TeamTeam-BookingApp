89af94b19561e1883a67882d7a2f7bf5821b39d4
Tom Hurford <hurford.t0@gmail.com>
2023-01-25 15:49:09 +0000

    Changes to structure

--- ui/src/components/App.js
+++ ui/src/components/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from '../assets/logo.svg';
+import '../styles/App.css';
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <header>This is a new app</header>
         <a
           className="App-link"
           href="https://reactjs.org"

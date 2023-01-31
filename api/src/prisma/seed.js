e0c8895e19edc26bfd1eb11323104c223c950a8e
Tom Hurford <hurford.t0@gmail.com>
2023-01-31 11:10:39 +0000

    Database made need to add documentation

---
+++ api/src/prisma/seed.js
@@ -0,0 +1,16 @@
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
+
+async function main() {
+    // Seed your database here
+}
+
+main()
+    .then(async() => {
+        await prisma.$disconnect()
+    })
+    .catch(async e => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })

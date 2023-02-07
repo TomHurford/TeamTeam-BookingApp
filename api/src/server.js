3a2f4585fb167f2ae605ed221e2f449253d91e8b
Tom Hurford <hurford.t0@gmail.com>
2023-02-07 19:22:08 +0000

    Allowed CORS

--- api/src/server.js
+++ api/src/server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 
 
@@ -14,6 +15,8 @@ var swaggerDocument = require('./swagger.json');
 
 var app = express();
 
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

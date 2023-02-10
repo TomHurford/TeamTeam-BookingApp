155abde8c9690649a31833fdef2218c06bf5f715
muzami1ali <muzamilalibosan@icloud.com>
2023-02-10 14:23:21 +0000

    Update api/src/server.js

--- api/src/server.js
+++ api/src/server.js
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const swaggerUi = require('swagger-ui-express');
-
-
-// If you are adding a new route, you need to add it here
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var swaggerDocument = require('./swagger.json');
-var basketRouter = require('./routes/basket');
-
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// If you are adding a new route, you need to add it here
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/basket', basketRouter);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
-=======
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -98,4 +51,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
->>>>>>> dev/client/events_pages

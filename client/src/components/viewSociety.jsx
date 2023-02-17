08627772774ddcb0c6bf5731dd172af1da8b8aaf
Nidhushkar Raveelackshman <r.nidhu31@gmail.com>
2023-02-17 23:21:41 +0000

    minor changes

--- client/src/components/viewSociety.jsx
+++ client/src/components/viewSociety.jsx
@@ -3,7 +3,62 @@ import { useParams } from "react-router-dom";
 
 const ViewSociety = () => {
   const { name } = useParams();
-  return <h1>Society: {name}</h1>;
+
+  return (
+    <React.Fragment>
+      <div className="header">
+        <h1>Society: {name}</h1>
+      </div>
+
+      <div className="row">
+        <div className="leftcolumn">
+          <div className="card">
+            <h2>TITLE HEADING</h2>
+            <h5>Title description, Dec 7, 2017</h5>
+            <div className="fakeimg" style={{ height: "200px" }}>
+              Image
+            </div>
+            <p>Some text..</p>
+          </div>
+          <div className="card">
+            <h2>TITLE HEADING</h2>
+            <h5>Title description, Sep 2, 2017</h5>
+            <div className="fakeimg" style={{ height: "200px" }}>
+              Image
+            </div>
+            <p>Some text..</p>
+          </div>
+        </div>
+        <div className="rightcolumn">
+          <div className="card">
+            <h2>About Me</h2>
+            <div className="fakeimg" style={{ height: "100px" }}>
+              Image
+            </div>
+            <p>
+              Some text about me in culpa qui officia deserunt mollit anim..
+            </p>
+          </div>
+          <div className="card">
+            <h3>Popular Post</h3>
+            <div className="fakeimg">Image</div>
+            <br />
+            <div className="fakeimg">Image</div>
+            <br />
+            <div className="fakeimg">Image</div>
+          </div>
+          <div className="card">
+            <h3>Follow Me</h3>
+            <p>Some text..</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer">
+        <h2>Footer</h2>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default ViewSociety;
--- client/src/societies/fakeSocieties.js
+++ client/src/societies/fakeSocieties.js
@@ -7,6 +7,8 @@ const societies = [
     category: { id: "1", name: "Sport" },
     numberOfFollowers: 888,
     numberOfEvents: 2,
+    socialLinks: { instagram: "", facebook: "", twitter: "", website: "" },
+    imageLinks: { logo: "", banner: "" },
   },
   {
     id: "2",
@@ -14,6 +16,8 @@ const societies = [
     category: { id: "1", name: "Sport" },
     numberOfFollowers: 2,
     numberOfEvents: 1,
+    socialLinks: { instagram: "", facebook: "", twitter: "", website: "" },
+    imageLinks: { logo: "", banner: "" },
   },
   {
     id: "3",
@@ -21,6 +25,8 @@ const societies = [
     category: { id: "3", name: "Social" },
     numberOfFollowers: 9999,
     numberOfEvents: 42,
+    socialLinks: { instagram: "", facebook: "", twitter: "", website: "" },
+    imageLinks: { logo: "", banner: "" },
   },
   {
     id: "4",
@@ -28,6 +34,8 @@ const societies = [
     category: { id: "2", name: "Academic" },
     numberOfFollowers: 888,
     numberOfEvents: 2,
+    socialLinks: { instagram: "", facebook: "", twitter: "", website: "" },
+    imageLinks: { logo: "", banner: "" },
   },
   {
     id: "5",
@@ -35,6 +43,8 @@ const societies = [
     category: { id: "1", name: "Sport" },
     numberOfFollowers: 222,
     numberOfEvents: 12,
+    socialLinks: { instagram: "", facebook: "", twitter: "", website: "" },
+    imageLinks: { logo: "", banner: "" },
   },
   {
     id: "6",
@@ -42,9 +52,15 @@ const societies = [
     category: { id: "3", name: "Social" },
     numberOfFollowers: 9999,
     numberOfEvents: 42,
+    socialLinks: { instagram: "", facebook: "", twitter: "", website: "" },
+    imageLinks: { logo: "", banner: "" },
   },
 ];
 
 export function getSocieties() {
   return societies;
+}
+
+export function getSociety(id) {
+  return societies.find((s) => s.id === id);
 }

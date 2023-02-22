b21b64fdb71563d83cceffa381a972ed4a6f5981
Ryan <ojirimu@gmail.com>
2023-02-22 10:27:59 +0000

    edit contacts page

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,10 +1,15 @@
-import React from "react";
-import '../styles/TitleOfPage.css';
+import React from 'react';
+import '../styles/Contact.css';
 //A component for Contact page to be added
 
 function Contact(){
     return(
-        <h1 className="title">I am Contact Page</h1>
+        <div className='page-container'>
+            <div className='underlay'></div>
+            <h1 className='title'>Who are we</h1>
+            <p className='data'>we are an event booking application with the aim to help consumers have access to events avaiable</p>
+            <p className='data'>email:ticketopiacontact@gmail.com</p>
+        </div>
     )
 }
 export default Contact

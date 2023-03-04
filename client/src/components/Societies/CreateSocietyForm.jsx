7132e8d18ad2dfd42a6acdc29475b39e0a158c44
Nidhushkar Raveelackshman <r.nidhu31@gmail.com>
2023-03-04 01:18:00 +0000

    reworked how createSocietyForm gets validated+handled

--- client/src/components/Societies/CreateSocietyForm.jsx
+++ client/src/components/Societies/CreateSocietyForm.jsx
@@ -1,12 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import Input from "../common/Input";
-import Joi from "joi-browser";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
-class CreateSocietyForm extends Component {
-  state = {
-    society: {
-      name: "",
-      category: "",
+function CreateSocietyForm() {
+  const formik = useFormik({
+    initialValues: {
+      societyName: "",
+      category: "Sports",
       description: "",
       website: "",
       instagram: "",
@@ -15,116 +16,158 @@ class CreateSocietyForm extends Component {
       logo: "",
       banner: "",
     },
-  };
 
-  handleSubmit = (e) => {
-    e.preventDefault();
+    validationSchema: Yup.object({
+      societyName: Yup.string().required("Society name is required"),
+      description: Yup.string()
+        .min(50, "Society description must be at least 50 characters.")
+        .required("Society description is required"),
+      website: Yup.string().url("Must be a valid URL"),
+      instagram: Yup.string().url("Must be a valid URL"),
+      twitter: Yup.string().url("Must be a valid URL"),
+      facebook: Yup.string().url("Must be a valid URL"),
+      logo: Yup.string().url("Must be a valid URL"),
+      banner: Yup.string().url("Must be a valid URL"),
+    }),
 
-    console.log("Society created");
-  };
+    onSubmit: (values) => {
+      console.log("Society created");
+    },
+  });
 
-  handleChange = ({ currentTarget: input }) => {
-    const society = { ...this.state.society };
-    society[input.name] = input.value;
-    this.setStae({ society });
-  };
+  return (
+    <div>
+      <h1>Create Society</h1>
 
-  render() {
-    const { society } = this.state;
-    return (
-      <div>
-        <h1>Create Society</h1>
-        <form onSubmit={this.handleSubmit}>
-          <Input
-            name="societyName"
-            value={society.name}
-            label="Society Name"
-            onChange={this.handleChange}
-          />
+      <form onSubmit={formik.handleSubmit}>
+        <Input
+          name="societyName"
+          value={formik.values.societyName}
+          onChange={formik.handleChange}
+          label={
+            formik.touched.societyName && formik.errors.societyName
+              ? formik.errors.societyName
+              : "Society Name"
+          }
+        />
 
-          <div className="form-group">
-            <label>Select a category</label>
-            <select
-              value={society.category}
-              onChange={this.handleChange}
-              id="societyCategory"
-              name="societyCategory"
-              className="form-select"
-            >
-              <option value="1" defaultValue>
-                Sports
-              </option>
-              <option value="2">Academic</option>
-              <option value="3">Social</option>
-              <option value="4">Other</option>
-            </select>
-          </div>
+        <div className="form-group">
+          <label>Select a category</label>
+          <select
+            id="category"
+            name="category"
+            value={formik.values.category}
+            onChange={formik.handleChange}
+            className="form-select"
+          >
+            <option defaultValue>Sports</option>
+            <option>Academic</option>
+            <option>Social</option>
+            <option>Other</option>
+          </select>
+        </div>
 
-          {/* Socials below*/}
-          <h5>Socials</h5>
-          <Input
-            name="societyWebsite"
-            value={society.website}
-            label="Society Website"
-            onChange={this.handleChange}
-          />
-          <Input
-            name="instagram"
-            value={society.instagram}
-            label="Society Instagram"
-            onChange={this.handleChange}
-          />
-          <Input
-            name="twitter"
-            value={society.twitter}
-            label="Society Twitter"
-            onChange={this.handleChange}
-          />
-          <Input
-            name="facebook"
-            value={society.facebook}
-            label="Society Facebook"
-            onChange={this.handleChange}
-          />
+        {/* Socials below*/}
+        <h5>Socials</h5>
+        <Input
+          name="website"
+          value={formik.values.website}
+          onChange={formik.handleChange}
+          label={
+            formik.touched.website && formik.errors.website
+              ? formik.errors.website
+              : "Website"
+          }
+          //onBlur={formik.handleBlur}
+        />
+        <Input
+          name="instagram"
+          value={formik.values.instagram}
+          onChange={formik.handleChange}
+          label={
+            formik.touched.instagram && formik.errors.instagram
+              ? formik.errors.instagram
+              : "Instagram"
+          }
+        />
+        <Input
+          name="twitter"
+          value={formik.values.twitter}
+          onChange={formik.handleChange}
+          label={
+            formik.touched.twitter && formik.errors.twitter
+              ? formik.errors.twitter
+              : "Twitter"
+          }
+        />
+        <Input
+          name="facebook"
+          value={formik.values.facebook}
+          onChange={formik.handleChange}
+          label={
+            formik.touched.facebook && formik.errors.facebook
+              ? formik.errors.facebook
+              : "Facebook"
+          }
+        />
 
-          {/* Image links below*/}
-          <h5>Images</h5>
-          <small id="imageHelp" className="form-text text-muted">
-            You must enter a link to the images
-          </small>
-          <Input
-            name="societyLogo"
-            value={society.logo}
-            label="Society Logo"
-            onChange={this.handleChange}
-          />
-          <Input
-            name="societyBanner"
-            value={society.banner}
-            label="Society Banner"
-            onChange={this.handleChange}
-          />
+        {/* Image links below*/}
+        <h5>Images</h5>
+        <small id="imageHelp" className="form-text text-muted">
+          You must enter a link to the images
+        </small>
+        <Input
+          name="logo"
+          value={formik.values.logo}
+          onChange={formik.handleChange}
+          label={
+            formik.touched.logo && formik.errors.logo
+              ? formik.errors.logo
+              : "Society Logo"
+          }
+        />
+        <Input
+          name="banner"
+          value={formik.values.banner}
+          onChange={formik.handleChange}
+          label={
+            formik.touched.banner && formik.errors.banner
+              ? formik.errors.banner
+              : "Society Banner"
+          }
+        />
 
-          <div className="form-group">
-            <label htmlFor="">Society Description</label>
-            <textarea
-              id="societyDescription"
-              name="societyDescription"
-              value={society.description}
-              className="form-control"
-              type="text"
-              onChange={this.handleChange}
-              rows="8"
-              cols="173"
-              style={{ marginBottom: "8px" }}
-            ></textarea>
-          </div>
+        <div className="form-group">
+          <label
+            className={`form-label ${
+              formik.touched.instagram && formik.errors.description
+                ? " text-danger"
+                : ""
+            }`}
+            htmlFor="description"
+          >
+            {formik.touched.description && formik.errors.description
+              ? formik.errors.description
+              : "Society Description"}
+          </label>
+          <textarea
+            id="description"
+            name="description"
+            value={formik.values.description}
+            onChange={formik.handleChange}
+            type="text"
+            className="form-control"
+            onBlur={formik.handleBlur}
+            rows="8"
+            cols="173"
+            style={{ marginBottom: "8px" }}
+          ></textarea>
+        </div>
 
-          <button className="btn btn-primary">Create Society</button>
-        </form>
-      </div>
-    );
-  }
+        <button className="btn btn-primary">Create Society</button>
+      </form>
+    </div>
+  );
 }
 
 export default CreateSocietyForm;
--- client/src/components/common/Input.jsx
+++ client/src/components/common/Input.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 
-const Input = ({ name, label, value, onChange }) => {
+const Input = ({ name, label, value, onChange, error }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
       <input
-        value={value}
-        onChange={onChange}
         id={name}
         name={name}
+        value={value}
+        onChange={onChange}
         type="text"
         className="form-control"
       />
+      {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
 };

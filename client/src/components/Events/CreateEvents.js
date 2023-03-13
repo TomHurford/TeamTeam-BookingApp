8526d8b216b01f688b93327b2c35aa0ce62e4d28
Nidhushkar Raveelackshman <r.nidhu31@gmail.com>
2023-03-13 00:48:01 +0000

    got error messages working apart from for ticket info

--- client/src/components/Events/CreateEvents.js
+++ client/src/components/Events/CreateEvents.js
@@ -69,13 +69,15 @@ function CreateEvents() {
             <div className="form-group">
               <label
                 className={`form-label ${
-                  formikProps.eventName && formikProps.errors.eventName
+                  formikProps.errors.eventName && formikProps.touched.eventName
                     ? " text-danger"
                     : ""
                 }`}
                 htmlFor="eventName"
               >
-                Event Name:
+                {formikProps.touched.eventName && formikProps.errors.eventName
+                  ? formikProps.errors.eventName
+                  : "Event Name"}
               </label>
               <input
                 name="eventName"
@@ -86,54 +88,118 @@ function CreateEvents() {
                 onBlur={formikProps.handleBlur}
               />
             </div>
+
             <div className="form-group">
-              <label>Event Description:</label>
+              <label
+                className={`form-label ${
+                  formikProps.errors.description &&
+                  formikProps.touched.description
+                    ? " text-danger"
+                    : ""
+                }`}
+                htmlFor="description"
+              >
+                {formikProps.touched.description &&
+                formikProps.errors.description
+                  ? formikProps.errors.description
+                  : "Event Description"}
+              </label>
               <textarea
                 name="description"
                 value={formikProps.values.description}
                 onChange={formikProps.handleChange}
                 type="text"
                 className="form-control"
+                onBlur={formikProps.handleBlur}
               ></textarea>
             </div>
+
             <div className="form-group">
-              <label>Event Date:</label>
+              <label
+                className={`form-label ${
+                  formikProps.errors.date && formikProps.touched.date
+                    ? " text-danger"
+                    : ""
+                }`}
+                htmlFor="date"
+              >
+                {formikProps.touched.date && formikProps.errors.date
+                  ? formikProps.errors.date
+                  : "Event Date"}
+              </label>
               <input
                 name="date"
                 value={formikProps.values.date}
                 onChange={formikProps.handleChange}
                 type="date"
                 className="form-control"
+                onBlur={formikProps.handleBlur}
               />
             </div>
             <div className="form-group">
-              <label>Event Time:</label>
+              <label
+                className={`form-label ${
+                  formikProps.errors.time && formikProps.touched.time
+                    ? " text-danger"
+                    : ""
+                }`}
+                htmlFor="time"
+              >
+                {formikProps.touched.time && formikProps.errors.time
+                  ? formikProps.errors.time
+                  : "Event Time"}
+              </label>
               <input
                 name="time"
                 value={formikProps.values.time}
                 onChange={formikProps.handleChange}
                 type="time"
                 className="form-control"
+                onBlur={formikProps.handleBlur}
               />
             </div>
             <div className="form-group">
-              <label>Event Location:</label>
+              <label
+                className={`form-label ${
+                  formikProps.errors.location && formikProps.touched.location
+                    ? " text-danger"
+                    : ""
+                }`}
+                htmlFor="location"
+              >
+                {formikProps.touched.location && formikProps.errors.location
+                  ? formikProps.errors.location
+                  : "Event Location"}
+              </label>
               <input
                 name="location"
                 value={formikProps.values.location}
                 onChange={formikProps.handleChange}
                 type="text"
                 className="form-control"
+                onBlur={formikProps.handleBlur}
               />
             </div>
             <div className="form-group">
-              <label>Society ID:</label>
+              <label
+                className={`form-label ${
+                  formikProps.errors.societyId && formikProps.touched.societyId
+                    ? " text-danger"
+                    : ""
+                }`}
+                htmlFor="societyId"
+              >
+                {formikProps.touched.societyId && formikProps.errors.societyId
+                  ? formikProps.errors.societyId
+                  : "Society ID"}
+              </label>
               <input
                 name="societyId"
                 value={formikProps.values.societyId}
                 onChange={formikProps.handleChange}
                 type="text"
                 className="form-control"
+                onBlur={formikProps.handleBlur}
               />
             </div>
 
@@ -162,12 +228,26 @@ function CreateEvents() {
                       <Field name={`ticketInfo.${index}.ticketName`}>
                         {(fieldProps) => (
                           <div className="form-group">
-                            <label>Ticket Type Name:</label>
+                            <label
+                              className={`form-label ${
+                                formikProps.errors.ticketName &&
+                                formikProps.touched.ticketName
+                                  ? " text-danger"
+                                  : ""
+                              }`}
+                              htmlFor="ticketName"
+                            >
+                              {formikProps.touched.ticketName &&
+                              formikProps.errors.ticketName
+                                ? formikProps.errors.ticketName
+                                : "Ticket Type Name"}
+                            </label>
                             <input
                               name="ticketName"
                               onChange={formikProps.handleChange}
                               type="text"
                               className="form-control"
+                              onBlur={formikProps.handleBlur}
                               {...fieldProps.field}
                             ></input>
                           </div>
@@ -177,12 +257,26 @@ function CreateEvents() {
                       <Field name={`ticketInfo.${index}.price`}>
                         {(fieldProps) => (
                           <div className="form-group">
-                            <label>Ticket Price:</label>
+                            <label
+                              className={`form-label ${
+                                formikProps.errors.price &&
+                                formikProps.touched.price
+                                  ? " text-danger"
+                                  : ""
+                              }`}
+                              htmlFor="price"
+                            >
+                              {formikProps.touched.price &&
+                              formikProps.errors.price
+                                ? formikProps.errors.price
+                                : "Ticket Price"}
+                            </label>
                             <input
                               name="price"
                               onChange={formikProps.handleChange}
                               type="number"
                               className="form-control"
+                              onBlur={formikProps.handleBlur}
                               {...fieldProps.field}
                             ></input>
                           </div>
@@ -192,12 +286,26 @@ function CreateEvents() {
                       <Field name={`ticketInfo.${index}.quantity`}>
                         {(fieldProps) => (
                           <div className="form-group">
-                            <label>Ticket Quantity Available:</label>
+                            <label
+                              className={`form-label ${
+                                formikProps.errors.quantity &&
+                                formikProps.touched.quantity
+                                  ? " text-danger"
+                                  : ""
+                              }`}
+                              htmlFor="quantity"
+                            >
+                              {formikProps.touched.quantity &&
+                              formikProps.errors.quantity
+                                ? formikProps.errors.quantity
+                                : "Ticket Quantity Available"}
+                            </label>
                             <input
                               name="quantity"
                               onChange={formikProps.handleChange}
                               type="number"
                               className="form-control"
+                              onBlur={formikProps.handleBlur}
                               {...fieldProps.field}
                             ></input>
 

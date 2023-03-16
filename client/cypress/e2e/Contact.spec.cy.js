54d05f78b9b7d3741e31d7776aabc24e4bf9782d
Anirudh George <anirudhgeorge12@gmail.com>
2023-03-16 22:06:49 +0000

    Additional tests for contact form

--- client/cypress/e2e/Contact.spec.cy.js
+++ client/cypress/e2e/Contact.spec.cy.js
@@ -4,14 +4,42 @@ describe('Contact', () => {
     cy.get('h1').contains('Who are we')
     cy.get('p').contains('Ticketopia is...')
   })
-  it('Enter invalid details', () => {
+  it('Enter invalid name', () => {
     cy.visit('/contact')
     cy.get('input[name="customerName"]')
-    cy.get('input[name="customerEmail"]').type('john@')
-    cy.get('input[name="messageSubject"]')
-    cy.get('textarea[name="message"]').type('Test')
+    cy.get('input[name="customerEmail"]').type('john@email.com')
+    cy.get('input[name="messageSubject"]').type('Test')
+    cy.get('textarea[name="message"]').type('Test message  description  here ................. ')
     cy.get('button').click()
   })
+
+  it('Enter invalid email', () => {
+    cy.visit('/contact')
+    cy.get('input[name="customerName"]').type('John')
+    cy.get('input[name="customerEmail"]')
+    cy.get('input[name="messageSubject"]').type('Test')
+    cy.get('textarea[name="message"]').type('Test  message  description  here ................. ')
+    cy.get('button').click()
+  })
+
+  it('Enter invalid subject', () => {
+    cy.visit('/contact')
+    cy.get('input[name="customerName"]').type('John')
+    cy.get('input[name="customerEmail"]').type('john@email.com')
+    cy.get('input[name="messageSubject"]')
+    cy.get('textarea[name="message"]').type('Test  message  description  here ................. ')
+    cy.get('button').click()
+  })
+
+  it('Enter invalid query', () => {
+    cy.visit('/contact')
+    cy.get('input[name="customerName"]').type('John')
+    cy.get('input[name="customerEmail"]').type('john@email.com')
+    cy.get('input[name="messageSubject"]').type('Test')
+    cy.get('textarea[name="message"]')
+    cy.get('button').click()
+  })
+
   it('Enter valid details', () => {
     cy.visit('/contact')
     cy.get('input[name="customerName"]').type('John')

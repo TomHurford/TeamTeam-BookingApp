8303323a2e6beafbd1df1c7f377c2fddef4404dc
muzami1ali <muzamilalibosan@icloud.com>
2023-03-17 19:26:22 +0000

    Update EventsLogic

--- client/src/utils/EventsLogic.js
+++ client/src/utils/EventsLogic.js
@@ -1,62 +1,63 @@
 // This file contains functions that make API calls to the backend
 
-const axios = require('axios');
-//const axios = require('axios').default;
 const jwtController = require('./jwt.js');
 
 // This function gets all events from the backend
 export const getEvents = async () => {
-  try{
-    const response = await axios.get('http://localhost:5001/events', {})
-    const eventsArray = await response.data;
-    const events = eventsArray.events;
-    return events;
-  } catch (error) {
-    console.log(error)
-    return null;
-  }
-}
-
-// This function gets a specific event from the backend
-export const getEventById = async (eventId) => {
-  console.log('err')
-  try{
-    // const response = await axios.post('http://localhost:5001/events', {eventId: eventId});
-    const response = await fetch('http://localhost:5001/events', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + jwtController.getToken()
-      },
-      body: { eventId: eventId }
-    })
-    console.log(response);
-    const event = await response.data;
-    return event;
-  } catch (error) {
-    console.log(error)
-    return null;
-  }
-}
-
-/*
-export const checkPrivileges = async () => {
-  fetch('http://localhost:5001/events/auth', {
-    method: 'POST',
+  var events = [];
+  await fetch('http://localhost:5001/events', {
+    method: 'GET',
+    mode: 'cors',
     headers: {
-      'Authorization': 'Bearer ' + jwtController.getToken()
-    }
+      'Content-Type': 'application/json',
+    },
   })
-  .then(response => {
-    console.log(response);
-    if (response.status == 200) {
-      return true;
-    } else {
-      return false;
-    }
+  .then(response => response.json())
+  .then(data => {
+    events = data.events;
   })
   .catch(error => {
     console.log(error);
   })
+  return events;
 }
-*/
+
+// This function gets a specific event from the backend
+export const getEventById = async (eventId) => {
+  var event = {};
+  if(jwtController.getToken() != null){
+    await fetch('http://localhost:5001/events/', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + jwtController.getToken()
+      },
+      body: JSON.stringify({"eventId": eventId})
+    })
+    .then(response => response.json())
+    .then(data => {
+      event = data;
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  } else {
+    await fetch('http://localhost:5001/events/', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({"eventId": eventId})
+    })
+    .then(response => response.json())
+    .then(data => {
+      event = data;
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+  return event;
+}

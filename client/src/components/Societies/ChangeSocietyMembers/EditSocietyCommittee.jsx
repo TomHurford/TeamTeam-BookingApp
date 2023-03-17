a582b18b38d9bf2be1c679e972ee6b6af616a791
Nidhushkar Raveelackshman <r.nidhu31@gmail.com>
2023-03-17 19:29:55 +0000

    got view committee members working

--- client/src/components/Societies/ChangeSocietyMembers/EditSocietyCommittee.jsx
+++ client/src/components/Societies/ChangeSocietyMembers/EditSocietyCommittee.jsx
@@ -1,48 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Member from "./Member";
 import AddCommitteeMember from "./AddCommitteeMember";
+
 const jwtController = require("../../../utils/jwt.js");
+import axios from "axios";
 
 const EditSocietyCommittee = (props) => {
-  const [members, setMembers] = React.useState([
-    {
-      email: "nid@nid.com",
-      userId: 1,
-      societyId: props.societyId,
-      role: "admin",
-    },
-    { email: "hi@hi.com", userId: 2 },
-    { email: "hdsdfasi@dhi.com", userId: 3 },
-  ]);
+  // const [members, setMembers] = React.useState([
+  //   {
+  //     email: "nid@nid.com",
+  //     userId: 1,
+  //     role: "admin",
+  //   },
+
+  const [members, setMembers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.post(
+          "http://localhost:5001/societies/getCommitteeMembers",
+          {
+            societyId: props.societyId,
+          }
+        );
+        setMembers(response.data.committee);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [props.societyId]);
+
+  useEffect(() => {
+    setMembers(members);
+  }, [members]);
 
   const [newMemberId, setNewMemberId] = React.useState(4);
 
   const handleRemoveMember = (id) => {
-    if (members.length > 1) {
-      setMembers((prevMembers) => prevMembers.filter((m) => m.userId !== id));
-    } else {
-      console.log("Committee must have at least one member");
-    }
+    // if (members.length > 1) {
+    //   setMembers((prevMembers) => prevMembers.filter((m) => m.userId !== id));
+    // } else {
+    //   console.log("Committee must have at least one member");
+    // }
+    // const data = {
+    //   userId: parseInt(members.userId),
+    // };
+    // console.log(members);
+    // console.log(jwtController.getToken());
+    // console.log(data);
+    // fetch("http://localhost:5001/societies/removeCommitteeMember", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Authorization: "Bearer " + jwtController.getToken(),
+    //   },
+    //   body: JSON.stringify(data),
+    // })
+    //   .then((res) => res.status)
+    //   .then((status) => {
+    //     if (status === 200) {
+    //       alert("Member deleted successfully!");
+    //     } else {
+    //       alert("Error removing member");
+    //     }
+    //   });
   };
 
   const handleAddMember = (email) => {
-    setMembers((prevMembers) => [
-      ...prevMembers,
-      {
-        email: email,
-        userId: newMemberId,
-        societyId: props.societyId,
-        role: "newMember",
-      },
-    ]);
-    setNewMemberId((prevId) => prevId + 1);
-
     const data = {
       email: email,
-      userId: newMemberId,
       societyId: parseInt(props.societyId),
-      role: "newMember",
+      role: "blahblah",
     };
     console.log(jwtController.getToken());
 
@@ -63,6 +94,16 @@ const EditSocietyCommittee = (props) => {
           alert("Error adding member");
         }
       });
+
+    setMembers((prevMembers) => [
+      ...prevMembers,
+      {
+        email: email,
+        userId: newMemberId,
+        role: "blahblah",
+      },
+    ]);
+    setNewMemberId((prevId) => prevId + 1);
   };
 
   return (

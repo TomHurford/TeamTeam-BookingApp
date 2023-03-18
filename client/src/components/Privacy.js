44a003e7eda440d939361cd29ce859c014eb7333
Nidhushkar Raveelackshman <r.nidhu31@gmail.com>
2023-03-18 19:37:25 +0000

    added some text to static pages

--- client/src/components/Privacy.js
+++ client/src/components/Privacy.js
@@ -1,14 +1,51 @@
-import React from 'react';
-import '../styles/Privacy.css';
+import React from "react";
+import "../styles/Privacy.css";
 //A component for Contact page to be added
 
-function Privacy(){
-    return(
-        <div className='page-container'>
-            <div className='underlay'></div>
-            <h1 className='title'>Privacy</h1>
-            <p className='data'>Privacy policy here on cookies ask team on info of what our tokens collect</p>
-        </div>
-    )
+function Privacy() {
+  return (
+    <div className="page-container">
+      <div className="underlay"></div>
+      <h1 className="title">Privacy</h1>
+      <p className="data">
+        <center>
+          At our platform, we take your privacy seriously and are committed to
+          protecting your personal information. This privacy policy outlines the
+          types of information we collect, how we use it, and the measures we
+          take to safeguard it.
+          <br />
+          <br />
+          We collect personal information such as your name, email address, and
+          payment information when you sign up for an account, purchase tickets,
+          or use our platform. This information is used to process your
+          transactions, provide customer support, and improve our services.
+          <br />
+          <br />
+          We may also collect information about your use of our platform,
+          including IP address, browser type, and device information. This
+          information is used to analyze and improve our services and to
+          personalize your experience on our platform.
+          <br />
+          <br />
+          We may share your personal information with third-party service
+          providers, such as payment processors and marketing partners, to
+          facilitate transactions and improve our services. We will never sell
+          your personal information to third parties.
+          <br />
+          <br />
+          We take appropriate measures to safeguard your personal information,
+          including encryption, firewalls, and secure servers. We also require
+          our third-party service providers to implement appropriate security
+          measures to protect your personal information.
+          <br />
+          <br />
+          By using our platform, you consent to the collection and use of your
+          personal information as outlined in this privacy policy. If you have
+          any questions or concerns about our privacy practices, please contact
+          us using the contact page.
+        </center>
+      </p>
+    </div>
+  );
 }
-export default Privacy
+export default Privacy;
--- client/src/components/Terms.js
+++ client/src/components/Terms.js
@@ -1,15 +1,71 @@
-import React from 'react';
-import '../styles/Terms.css';
+import React from "react";
+import "../styles/Terms.css";
 //A component for Contact page to be added
 
-function Terms(){
-    return(
-        <div className='page-container'>
-            <div className='underlay'></div>
-            <h1 className='title'>Terms And Conditions</h1>
-            <p className='title'>Agreement to our legal terms</p>
-            <p className='data'>We operate the website WEBSITE, as well as any other<br></br>related productes and services that refer or link to these legal terms (the 'LEGAL TERMS')(collectivly, the 'SERVICES')<br></br><br></br>These Legal Terms constitute a legally binding agreement made between you, whether personally or behalf<br></br>of an entity("you"), and ticketopia, conserning your access to and use of the Services. You agree that by accessing the services, you <br></br> have read, unstood, and agreed to be bound by all these Legal Terms</p>
-        </div>
-    )
+function Terms() {
+  return (
+    <div className="page-container">
+      <div className="underlay"></div>
+      <h1 className="title">Terms And Conditions</h1>
+
+      <center>
+        <p>
+          Welcome to our ticket selling platform. By using our platform, you
+          agree to be bound by the following terms and conditions:
+        </p>
+        <p>
+          1. User Conduct: You agree to use our platform only for lawful
+          purposes and in accordance with these terms and conditions. You agree
+          not to use our platform for any illegal or unauthorized purpose.
+        </p>
+        <p>
+          2. Account Registration: You must create an account to use our
+          platform. You agree to provide accurate and complete information when
+          creating your account and to keep your login credentials confidential.
+          You are responsible for all activity that occurs under your account.
+        </p>
+        <p>
+          3. Event Creation and Ticket Sales: Society organizers are responsible
+          for creating and managing their events on our platform. You agree to
+          provide accurate and complete information when creating an event and
+          to comply with all applicable laws and regulations. You are
+          responsible for setting ticket prices, managing ticket inventory, and
+          processing refunds.
+        </p>
+        <p>
+          4. Payment Processing: Our platform uses third-party payment
+          processors to process transactions. You agree to pay all fees
+          associated with your use of our platform and to provide accurate and
+          complete payment information. We reserve the right to suspend or
+          terminate your account if payment is not received or is disputed.
+        </p>
+        <p>
+          5. Intellectual Property: Our platform and all content, features, and
+          functionality are owned by us or our licensors and are protected by
+          intellectual property laws. You agree not to copy, modify, distribute,
+          or reproduce any content from our platform without our prior written
+          consent.
+        </p>
+        <p>
+          6. Limitation of Liability: Our platform is provided on an "as is"
+          basis and we make no representations or warranties regarding its
+          operation or availability. We are not responsible for any damages,
+          including but not limited to direct, indirect, incidental, or
+          consequential damages, arising out of your use of our platform.
+        </p>
+        <p>
+          7. Termination: We reserve the right to suspend or terminate your
+          account at any time for any reason, without notice. You may also
+          terminate your account at any time by contacting us.
+        </p>
+
+        <p>
+          By using our platform, you agree to be bound by these terms and
+          conditions. If you have any questions or concerns about these terms
+          and conditions, please contact us at [insert contact information].
+        </p>
+      </center>
+    </div>
+  );
 }
-export default Terms
+export default Terms;

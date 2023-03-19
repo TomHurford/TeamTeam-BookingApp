8ddcc6f874fdd0c09809ca48669f394f3be71d26
muzami1ali <muzamilalibosan@icloud.com>
2023-03-19 13:54:26 +0000

    Update Purchase frontend and backend

--- client/src/components/Purchase.js
+++ client/src/components/Purchase.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from "react";
 import "../styles/Purchase.css";
 
+const jwtController = require("../utils/jwt.js");
+
 const FutureTickets = () => {
   const [futureTickets, setFutureTickets] = useState([]);
+  const [pastTickets, setPastTickets] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:5001/purchase/future", {
         method: 'POST',
-        body: JSON.stringify({}),
+        mode: 'cors',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          Authorization: "Bearer " + jwtController.getToken(),
         }
       })
       //present events from lowest to highest date
@@ -20,30 +24,73 @@ const FutureTickets = () => {
         );
         setFutureTickets(sortedTickets);
       });
+
+    fetch("http://localhost:5001/purchase", {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: "Bearer " + jwtController.getToken(),
+        }
+      })
+      //present events from lowest to highest date
+      .then((response) => response.json())
+      .then((data) => {
+        const sortedTickets = data.pastTickets.sort((a, b) =>
+          new Date(a.event.date) - new Date(b.event.date) 
+        );
+        setPastTickets(sortedTickets);
+      });
       
   }, []);
 
   return (
     <div data-testid='Purchase' className="page-container">
         <div className="underlay"></div>
-       <hr></hr><hr></hr> 
-      <h1>Upcoming Tickets</h1>
+      <h1>Past Event Purchases</h1>
       <table className="purchase">
         <thead>
           <tr>
             <th>Event Name</th>
             <th>Event Date</th>
-            <th>Status</th>
-            <th>Payment Method</th>
+            <th>Tickets</th>
           </tr>
         </thead>
         <tbody>
-          {futureTickets.map((ticket) => (
+          {pastTickets.length !==0 &&  pastTickets.map((ticket) => (
             <tr data-testid={ticket.id} key={ticket.id}>
-              <td>{ticket.event.name}</td>
+              <td><a href={"/event-details?eventId=" + ticket.event.id}>{ticket.event.name}</a></td>
               <td>{new Date(ticket.event.date).toLocaleDateString()}</td>
-              <td>{ticket.status}</td>
-              <td>{ticket.paymentMethod}</td>
+              {ticket.tickets.map((ticket) => (
+                <ul data-testid={ticket.id} key={ticket.id}>
+                  <li>Ticket ID: {ticket.id}</li>
+                  <li>Ticket Data: {ticket.ticketData}</li>
+                </ul>))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <h1>Upcoming Event Purchases</h1>
+      <table className="purchase">
+        <thead>
+          <tr>
+            <th>Event Name</th>
+            <th>Event Date</th>
+            <th>Tickets</th>
+          </tr>
+        </thead>
+        <tbody>
+          {futureTickets.length !==0 &&  futureTickets.map((ticket) => (
+            <tr data-testid={ticket.id} key={ticket.id}>
+              <td><a href={"/event-details?eventId=" + ticket.event.id}>{ticket.event.name}</a></td>
+              <td>{new Date(ticket.event.date).toLocaleDateString()}</td>
+              <td>
+              {ticket.tickets.map((ticket) => (
+                <ul data-testid={ticket.id} key={ticket.id}>
+                  <li>Ticket ID: {ticket.id}</li>
+                  <li>Ticket Data: {ticket.ticketData}</li>
+                </ul>))}
+              </td>
             </tr>
           ))}
         </tbody>

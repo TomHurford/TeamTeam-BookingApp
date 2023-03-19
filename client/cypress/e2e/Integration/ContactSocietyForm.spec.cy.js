ab5c6b46344368a2cf57e35febf0003426e2d18d
Anirudh George <anirudhgeorge12@gmail.com>
2023-03-19 10:54:20 +0000

    Rearrange in format for contact society form tests

--- client/cypress/e2e/Integration/ContactSocietyForm.spec.cy.js
+++ client/cypress/e2e/Integration/ContactSocietyForm.spec.cy.js
@@ -16,51 +16,52 @@ describe('Contact Society Form', () => {
     cy.contains('Submit query').click();
   });
 
-  it('Invalid name in contact society form', () => {
-    cy.visit('/society/1');
-    cy.contains('Your Email').parent().find('input').type('john.doe@email.com', { force: true });
-    cy.contains('Subject of your query').parent().find('input').type('Test Subject', { force: true });
-    cy.contains('Your query').parent().find('textarea').type('Test Message description here .....................', { force: true });
-    cy.contains('Submit query').click();
-    cy.contains('You must enter your name');
+  describe('Invalid inputs', () => {
+    it('Submit invalid name in contact society form', () => {
+      cy.visit('/society/1');
+      cy.contains('Your Email').parent().find('input').type('john.doe@email.com', { force: true });
+      cy.contains('Subject of your query').parent().find('input').type('Test Subject', { force: true });
+      cy.contains('Your query').parent().find('textarea').type('Test Message description here .....................', { force: true });
+      cy.contains('Submit query').click();
+      cy.contains('You must enter your name');
+    })
 
+    it('Submit invalid email in contact society form', () => {
+      cy.visit('/society/1');
+      cy.contains('Your Name').parent().find('input').type('John Doe', { force: true });
+      cy.contains('Your Email').parent().find('input').type('incorrectEmail', { force: true });
+      cy.contains('Subject of your query').parent().find('input').type('Test Subject', { force: true });
+      cy.contains('Your query').parent().find('textarea').type('Test Message description here .....................', { force: true });
+      cy.contains('customerEmail must be a valid email');
+      cy.contains('Submit query').click();
+    });
+
+    it('Submit empty subject in contact society form', () => {
+      cy.visit('/society/1');
+      cy.contains('Your Name').parent().find('input').type('John Doe', { force: true });
+      cy.contains('Your Email').parent().find('input').type('john.doe@email.com', { force: true });
+      cy.contains('Your query').parent().find('textarea').type('Test Message description here .....................', { force: true });
+      cy.contains('Submit query').click();
+      cy.contains('A subject is required');
+    });
+
+    it('Submit empty query in contact society form', () => {
+      cy.visit('/society/1');
+      cy.contains('Your Name').parent().find('input').type('John Doe', { force: true });
+      cy.contains('Your Email').parent().find('input').type('john.doe@email.com', { force: true });
+      cy.contains('Subject of your query').parent().find('input').type('Test Subject', { force: true });
+      cy.contains('Submit query').click();
+      cy.contains('A message is required');
+    });
+
+    it('Submit query with less than 50 characters in contact society form', () => {
+      cy.visit('/society/1');
+      cy.contains('Your Name').parent().find('input').type('John Doe', { force: true });
+      cy.contains('Your Email').parent().find('input').type('john.doe@email.com', { force: true });
+      cy.contains('Subject of your query').parent().find('input').type('Test Subject', { force: true });
+      cy.contains('Submit query').parent().find('textarea').type('Test Message description here', { force: true });
+      cy.contains('Submit query').click();
+      cy.contains('Message must be at least 50 characters long');
+    });
   })
-
-  it('Invalid email in contact society form', () => {
-    cy.visit('/society/1');
-    cy.contains('Your Name').parent().find('input').type('John Doe', { force: true });
-    cy.contains('Your Email').parent().find('input').type('incorrectEmail', { force: true });
-    cy.contains('Subject of your query').parent().find('input').type('Test Subject', { force: true });
-    cy.contains('Your query').parent().find('textarea').type('Test Message description here .....................', { force: true });
-    cy.contains('customerEmail must be a valid email').should('be.visible');
-    cy.contains('Submit query').click();
-  });
-
-  it('Empty subject in contact society form', () => {
-    cy.visit('/society/1');
-    cy.contains('Your Name').parent().find('input').type('John Doe', { force: true });
-    cy.contains('Your Email').parent().find('input').type('john.doe@email.com', { force: true });
-    cy.contains('Your query').parent().find('textarea').type('Test Message description here .....................', { force: true });
-    cy.contains('Submit query').click();
-    cy.contains('A subject is required');
-  });
-
-  it('Empty query in contact society form', () => {
-    cy.visit('/society/1');
-    cy.contains('Your Name').parent().find('input').type('John Doe', { force: true });
-    cy.contains('Your Email').parent().find('input').type('john.doe@email.com', { force: true });
-    cy.contains('Subject of your query').parent().find('input').type('Test Subject', { force: true });
-    cy.contains('Submit query').click();
-    cy.contains('A message is required');
-  });
-
-  it('Query with less than 50 characters in contact society form', () => {
-    cy.visit('/society/1');
-    cy.contains('Your Name').parent().find('input').type('John Doe', { force: true });
-    cy.contains('Your Email').parent().find('input').type('john.doe@email.com', { force: true });
-    cy.contains('Subject of your query').parent().find('input').type('Test Subject', { force: true });
-    cy.contains('Submit query').parent().find('textarea').type('Test Message description here', { force: true });
-    cy.contains('Submit query').click();
-    cy.contains('Message must be at least 50 characters long');
-  });
 });

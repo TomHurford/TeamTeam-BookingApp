7cb529f1a995a611eab9ed09bf4f2bd6e01d015b
muzami1ali <muzamilalibosan@icloud.com>
2023-03-20 19:35:20 +0000

    Update Forms validation for whitespace

--- client/src/components/Societies/CreateSocietyForm.jsx
+++ client/src/components/Societies/CreateSocietyForm.jsx
@@ -21,8 +21,12 @@ function CreateSocietyForm() {
     },
 
     validationSchema: Yup.object({
-      societyName: Yup.string().required("Society name is required"),
+      societyName: Yup.string()
+        .trim()
+        .min(3, "Society name must be at least 3 characters long")
+        .required("Society name is required"),
       description: Yup.string()
+        .trim()
         .min(50, "Society description must be at least 50 characters.")
         .required("Society description is required"),
       website: Yup.string().url("Must be a valid URL"),
@@ -31,7 +35,9 @@ function CreateSocietyForm() {
       facebook: Yup.string().url("Must be a valid URL"),
       logo: Yup.string().url("Must be a valid URL"),
       banner: Yup.string().url("Must be a valid URL"),
-      email: Yup.string().email().required("Must be a valid email address"),
+      email: Yup.string()
+        .email("Must be a valid email address")
+        .required("Must be a valid email address"),
     }),
 
     onSubmit: async (values) => {
@@ -111,9 +117,9 @@ function CreateSocietyForm() {
             className="form-control"
             onBlur={formik.handleBlur}
           />
-          {formik.touched.societyEmail && formik.errors.societyEmail ? (
-            <label className="errortext" htmlFor="societyEmail">
-              {formik.errors.societyEmail}
+          {formik.touched.email && formik.errors.email ? (
+            <label className="errortext" htmlFor="email">
+              {formik.errors.email}
               <br />
             </label>
           ) : (

d3ab24aee623ef490d973c60cbd905cef5c283b1
muzami1ali <muzamilalibosan@icloud.com>
2023-03-21 11:11:40 +0000

    Update EventDetails
    - Past events cannot be edited
    - Past events cannot display tickets

--- client/src/components/Events/EventDetails.js
+++ client/src/components/Events/EventDetails.js
@@ -111,9 +111,11 @@ class EventDetails extends Component {
               </a>
             </div>
           </div>
-
-          <div className="tickerHolder">
+          
+          {new Date() <= new Date(event.event.date) && 
+            (<div className="tickerHolder">
             <h2>Tickets</h2>
+            {console.log(new Date() <= event.event.date)}
             {event.event.ticketTypes.map((ticketType) => {
               return (
                 <TicketHolderTicket
@@ -146,7 +148,7 @@ class EventDetails extends Component {
               Edit Event
             </button>
             {/* </Link> */}
-          </div>
+          </div>)}
 
           <div className="spacer"></div>
         </div>

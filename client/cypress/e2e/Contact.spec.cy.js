fbb0a040d93bff46a5941273b640d16270550019
muzami1ali <muzamilalibosan@icloud.com>
2023-03-22 00:23:11 +0000

    Update contact forms cypress tests
    Co-authored-by: anirudhgeorge12 <114352572+anirudhgeorge12@users.noreply.github.com>

--- client/cypress/e2e/Contact.spec.cy.js
+++ client/cypress/e2e/Contact.spec.cy.js
@@ -15,10 +15,7 @@ describe('Contact', () => {
 
     it('Submit empty email', () => {
       cy.visit('/contact')
-      cy.get('input[name="customerName"]').type('John')
       cy.get('input[name="customerEmail"]')
-      cy.get('input[name="messageSubject"]').type('Test')
-      cy.get('textarea[name="message"]').type('Test  message  description  here ................. ')
       cy.get('button').click()
       cy.contains('You must enter your email')
     })
@@ -32,10 +29,7 @@ describe('Contact', () => {
 
     it('Submit empty subject', () => {
       cy.visit('/contact')
-      cy.get('input[name="customerName"]').type('John')
-      cy.get('input[name="customerEmail"]').type('john@email.com')
       cy.get('input[name="messageSubject"]')
-      cy.get('textarea[name="message"]').type('Test  message  description  here ................. ')
       cy.get('button').click()
       cy.contains('A subject is required')
     })
@@ -50,54 +44,67 @@ describe('Contact', () => {
 
 
   describe('Test invalid inputs', () => {
-  it('Enter invalid name', () => {
-    cy.visit('/contact')
-    cy.get('input[name="customerName"]')
-    cy.get('input[name="customerEmail"]').type('john@email.com')
-    cy.get('input[name="messageSubject"]').type('Test')
-    cy.get('textarea[name="message"]').type('Test message  description  here ................. ')
-    cy.get('button').click()
-    cy.contains('You must enter your name')
-  })
+    it('Enter invalid name', () => {
+      cy.visit('/contact')
+      cy.get('input[name="customerName"]')
+      cy.get('button').click()
+      cy.contains('You must enter your name')
+    })
 
-  it('Submit invalid email', () => {
-    cy.visit('/contact')
-    cy.get('input[name="customerName"]').type('John')
-    cy.get('input[name="customerEmail"]').type('johnemail.com')
-    cy.get('input[name="messageSubject"]').type('Test')
-    cy.get('textarea[name="message"]').type('Test  message  description  here ................. ')
-    cy.get('button').click()
-    cy.contains('customerEmail must be a valid email')
-  })
-  
+    it('Submit invalid email', () => {
+      cy.visit('/contact')
+      cy.get('input[name="customerEmail"]').type('johnemail.com')
+      cy.get('button').click()
+      cy.contains('customerEmail must be a valid email')
+    })
 
-  it('Enter invalid query', () => {
-    cy.visit('/contact')
-    cy.get('input[name="customerName"]').type('John')
-    cy.get('input[name="customerEmail"]').type('john@email.com')
-    cy.get('input[name="messageSubject"]').type('Test')
-    cy.get('textarea[name="message"]')
-    cy.get('button').click()
-    cy.contains('A message is required')
-  })
+    it('Enter invalid query', () => {
+      cy.visit('/contact')
+      cy.get('textarea[name="message"]')
+      cy.get('button').click()
+      cy.contains('A message is required')
+    })
 
-  it('Query less than 50 characters long', () => {
-    cy.visit('/contact')
-    cy.get('input[name="customerName"]').type('John')
-    cy.get('input[name="customerEmail"]').type('john@email.com')
-    cy.get('input[name="messageSubject"]').type('Test')
-    cy.get('textarea[name="message"]').type('Test message')
-    cy.get('button').click()
-    cy.contains('Message must be at least 50 characters long')
+    it('Query less than 50 characters long', () => {
+      cy.visit('/contact')
+      cy.get('textarea[name="message"]').type('Test message')
+      cy.get('button').click()
+      cy.contains('Message must be at least 50 characters long')
+    })
   })
-})
+  describe('Whitespace inputs', ()=>{
+    it('Whitespace input for name', ()=>{
+      cy.visit('/contact')
+      cy.get('input[name="customerName"]').type('    ')
+      cy.contains('Contact Us').click()
+      cy.contains('Name must be at least 3 characters long')
+    })
+    it('Whitespace input for email', ()=>{
+      cy.visit('/contact')
+      cy.get('input[name="customerEmail"]').type('    ')
+      cy.contains('Contact Us').click()
+      cy.contains('customerEmail must be a valid email')
+    })
+    it('Whitespace input for subject of query', ()=>{
+      cy.visit('/contact')
+      cy.get('input[name="messageSubject"]').type('    ')
+      cy.contains('Contact Us').click()
+      cy.contains('Subject must be at least 3 characters long')
+    })
+    it('Whitespace input for query', () =>{
+      cy.visit('/contact')
+      cy.get('textarea[name="message"]').type('    ')
+      cy.contains('Contact Us').click()
+      cy.contains('Message must be at least 50 characters long')
+    })
 
-  it('Enter valid details', () => {
-    cy.visit('/contact')
-    cy.get('input[name="customerName"]').type('John')
-    cy.get('input[name="customerEmail"]').type('john@email.com')
-    cy.get('input[name="messageSubject"]').type('Test')
-    cy.get('textarea[name="message"]').type('Test  message  description  here ................. ')
-    cy.get('button').click()
   })
+    it('Enter valid details', () => {
+      cy.visit('/contact')
+      cy.get('input[name="customerName"]').type('John')
+      cy.get('input[name="customerEmail"]').type('john@email.com')
+      cy.get('input[name="messageSubject"]').type('Test')
+      cy.get('textarea[name="message"]').type('Test  message  description  here ................. ')
+      cy.get('button').click()
+    })
 });

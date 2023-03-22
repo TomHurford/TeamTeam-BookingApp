2f818d3730ef3e7547223ec22023e500245c9e73
muzami1ali <muzamilalibosan@icloud.com>
2023-03-22 00:22:25 +0000

    Update Login and Basket cypress tests
    Co-authored-by: anirudhgeorge12 <114352572+anirudhgeorge12@users.noreply.github.com>

--- client/cypress/e2e/Integration/Basket.spec.cy.js
+++ client/cypress/e2e/Integration/Basket.spec.cy.js
@@ -1,17 +1,68 @@
-/**describe('Basket', () => {
-  it('Test basket without login', () => {
-    cy.visit('/');
+describe('Basket', () => {
 
-    // cy.get('nav[class="navbar"]').find('ul[class="right"]').find('li').eq(0).click();
+  it('Simulate purchase with login',() => {
+    cy.visit('/login')
+    cy.get('input[name="loginEmail"]').type('admin@admin.com')
+    cy.get('input[name="loginPassword"]').type('admin123')
+    cy.get('button[name="loginbutton"]').click()
+    cy.wait(500)
+    cy.visit('/event-details?eventId=1')
 
     cy.wait(500)
-    cy.contains('Event 1').click({force: true})
     cy.get("div[data-testid='right10']").click()
     cy.get("div[data-testid='right10']").click()
     cy.get("div[data-testid='right10']").click()
 
+    cy.wait(500)
     cy.contains('Go To Basket').click()
 
+    cy.wait(500)
+    cy.get("div[data-testid='left10']").click()
+
+    cy.contains('Checkout').click()
+
+    cy.wait(500)
+    cy.url().should('include', '/tickets')
+
+    cy.contains('Logout').click()
+  })
+
+  it('Simulate purchase without login',() => {
+    cy.visit('/event-details?eventId=1')
+
+    cy.wait(500)
+    cy.get("div[data-testid='right10']").click()
+    cy.get("div[data-testid='right10']").click()
+    cy.get("div[data-testid='right10']").click()
+
+    cy.wait(500)
+    cy.contains('Go To Basket').click()
+
+    cy.wait(500)
     cy.contains('Log In').click()
   });
-});*/
+
+  it('Add tickets of a different event to basket',() => {
+    cy.visit('/event-details?eventId=1')
+
+    cy.wait(500)
+    cy.get("div[data-testid='right10']").click()
+    cy.get("div[data-testid='right10']").click()
+    cy.get("div[data-testid='right10']").click()
+
+    cy.wait(500)
+
+    cy.visit('/event-details?eventId=3')
+    cy.get("div[data-testid='right10']").click()
+    cy.get("div[data-testid='right10']").click()
+    cy.get("div[data-testid='right10']").click()
+  })
+
+  it('Click remove ticket button on empty basket',() => {
+    cy.visit('/event-details?eventId=1')
+    cy.wait(500)
+
+    cy.get("div[data-testid='left10']").click()
+    cy.get("div[data-testid='left10']").click()
+  })
+})

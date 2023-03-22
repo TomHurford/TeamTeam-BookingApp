1eee0c1f3adda2c7883f21c3114ee72455fb20ec
Tom Hurford <hurford.t0@gmail.com>
2023-03-22 21:41:26 +0000

    Committee Tests

--- api/src/__tests__/committee.test.js
+++ api/src/__tests__/committee.test.js
@@ -23,14 +23,6 @@ beforeEach(async () => {
 
 describe('addCommitteeMember', () => {
   test('add committee member with valid token', async () => {
-    await prisma.committee.delete({
-      where: {
-        userId_societyId: {
-          societyId: 1,
-          userId: 2,
-        },
-      },
-    });
     const res = await request(app)
         .post('/societies/addCommitteeMember')
         .set('Authorization', `Bearer ` + token)
@@ -165,9 +157,8 @@ describe('removeCommitteeMember', () => {
         .set('Authorization', `Bearer ` + token)
         .send({
           societyId: 1,
-          email: 'student@kcl.ac.uk',
+          userId: 2,
         });
-    console.log(res.body);
     expect(res.statusCode).toBe(200);
     expect(res.body).not.toBeNull();
     expect(res.body.message).toBe('User removed from committee');
@@ -176,11 +167,276 @@ describe('removeCommitteeMember', () => {
   test('remove committee member with invalid token', async () => {
     const res = await request(app)
         .post('/societies/removeCommitteeMember')
-        .set('Authorization', `Bearer ` + 'invalid token');
+        .set('Authorization', `Bearer ` + 'invalid token')
+        .send({
+          societyId: 1,
+          userId: 2,
+        });
     expect(res.statusCode).toBe(500);
     // Check the response
     expect(res.body).not.toBeNull();
     expect(res.body.message).toBe('Internal Server Error');
+  });
+
+  test('remove committee member with missing parameters (userId)', async () => {
+    const res = await request(app)
+        .post('/societies/removeCommitteeMember')
+        .set('Authorization', `Bearer ` + token)
+        .send({
+          societyId: 1,
+        });
+    expect(res.statusCode).toBe(400);
+    // Check the response
+    expect(res.body).not.toBeNull();
+    expect(res.body.message).toBe('Invalid Request');
+  });
+
+  test('remove committee member with missing parameters (societyId)',
+      async () => {
+        const res = await request(app)
+            .post('/societies/removeCommitteeMember')
+            .set('Authorization', `Bearer ` + token)
+            .send({
+              userId: 2,
+            });
+        expect(res.statusCode).toBe(400);
+        // Check the response
+        expect(res.body).not.toBeNull();
+        expect(res.body.message).toBe('Invalid Request');
+      });
+
+  test('remove committee member with invalid societyId', async () => {
+    const res = await request(app)
+        .post('/societies/removeCommitteeMember')
+        .set('Authorization', `Bearer ` + token)
+        .send({
+          societyId: -1,
+          userId: 2,
+        });
+    expect(res.statusCode).toBe(400);
+    // Check the response
+    expect(res.body).not.toBeNull();
+    expect(res.body.message).toBe('Invalid Request');
+  });
+
+  test('remove committee member with invalid userId', async () => {
+    const res = await request(app)
+        .post('/societies/removeCommitteeMember')
+        .set('Authorization', `Bearer ` + token)
+        .send({
+          societyId: 1,
+          userId: -1,
+        });
+    expect(res.statusCode).toBe(400);
+    // Check the response
+    expect(res.body).not.toBeNull();
+    expect(res.body.message).toBe('Invalid Request');
+  });
+
+  test('remove committee member with invalid societyId (string)',
+      async () => {
+        const res = await request(app)
+            .post('/societies/removeCommitteeMember')
+            .set('Authorization', `Bearer ` + token)
+            .send({
+              societyId: 'string',
+              userId: 2,
+            });
+        expect(res.statusCode).toBe(400);
+        // Check the response
+        expect(res.body).not.toBeNull();
+        expect(res.body.message).toBe('Invalid Request');
+      });
+
+  test('remove committee member with invalid userId (string)',
+      async () => {
+        const res = await request(app)
+            .post('/societies/removeCommitteeMember')
+            .set('Authorization', `Bearer ` + token)
+            .send({
+              societyId: 1,
+              userId: 'string',
+            });
+        expect(res.statusCode).toBe(400);
+        // Check the response
+        expect(res.body).not.toBeNull();
+        expect(res.body.message).toBe('Invalid Request');
+      });
+
+  test('remove committee member with invalid userId (large number)',
+      async () => {
+        const res = await request(app)
+            .post('/societies/removeCommitteeMember')
+            .set('Authorization', `Bearer ` + token)
+            .send({
+              societyId: 1,
+              userId: 10000000,
+            });
+        expect(res.statusCode).toBe(400);
+        // Check the response
+        expect(res.body).not.toBeNull();
+        expect(res.body.message).toBe('User does not exist');
+      });
+
+  test('remove committee member with invalid societyId (large number)',
+      async () => {
+        const res = await request(app)
+            .post('/societies/removeCommitteeMember')
+            .set('Authorization', `Bearer ` + token)
+            .send({
+              societyId: 10000000,
+              userId: 2,
+            });
+        expect(res.statusCode).toBe(400);
+        // Check the response
+        expect(res.body).not.toBeNull();
+        expect(res.body.message).toBe('Society does not exist');
+      });
+
+  test('remove committee member who is not in the committee',
+      async () => {
+        // Check if user is in the committee
+        const user = await prisma.committee.findUnique({
+          where: {
+            userId_societyId: {
+              userId: 2,
+              societyId: 1,
+            },
+          },
+        });
+
+        if (user) {
+          await prisma.committee.delete({
+            where: {
+              userId_societyId: {
+                userId: 2,
+                societyId: 1,
+              },
+            },
+          });
+        }
+        const res = await request(app)
+            .post('/societies/removeCommitteeMember')
+            .set('Authorization', `Bearer ` + token)
+            .send({
+              societyId: 1,
+              userId: 2,
+            });
+        expect(res.statusCode).toBe(400);
+        // Check the response
+        expect(res.body).not.toBeNull();
+        expect(res.body.message).toBe('User is not a committee member');
+      });
+
+  test('remove committee member logged in as someone not in the committee',
+      async () => {
+        // Check if user is in the committee
+        const user = await prisma.committee.findUnique({
+          where: {
+            userId_societyId: {
+              userId: 2,
+              societyId: 1,
+            },
+          },
+        });
+
+        if (user) {
+          await prisma.committee.delete({
+            where: {
+              userId_societyId: {
+                userId: 2,
+                societyId: 1,
+              },
+            },
+          });
+        }
+
+        // Add a different user to the committee
+        await prisma.committee.create({
+          data: {
+            userId: 3,
+            societyId: 1,
+            role: 'Committee Member',
+          },
+        });
+        // Login as a user not in the committee
+        const res = await request(app)
+            .post('/user/login')
+            .send({
+              email: 'student@kcl.ac.uk',
+              password: 'student',
+            });
+        expect(res.statusCode).toBe(200);
+        // Check the response
+        expect(res.body).not.toBeNull();
+        expect(res.body.token).not.toBeNull();
+        const token = res.body.token;
+        // Make the request
+        const res2 = await request(app)
+            .post('/societies/removeCommitteeMember')
+            .set('Authorization', `Bearer ` + token)
+            .send({
+              societyId: 1,
+              userId: 3,
+            });
+        expect(res2.statusCode).toBe(400);
+        // Check the response
+        expect(res2.body).not.toBeNull();
+        expect(res2.body.message).toBe('Unauthorized');
+
+        // Remove the user from the committee
+        await prisma.committee.delete({
+          where: {
+            userId_societyId: {
+              userId: 3,
+              societyId: 1,
+            },
+          },
+        });
+      });
+
+  test('remove president from committee', async () => {
+    await prisma.committee.create({
+      data: {
+        societyId: 1,
+        userId: 2,
+        role: 'Committee Member',
+        isPresident: false,
+      },
+    });
+    // Login as not president
+    const res = await request(app)
+        .post('/user/login')
+        .send({
+          email: 'student@kcl.ac.uk',
+          password: 'student',
+        });
+    expect(res.statusCode).toBe(200);
+    // Check the response
+    expect(res.body).not.toBeNull();
+    expect(res.body.token).not.toBeNull();
+    const token = res.body.token;
+    // Make the request
+    const res2 = await request(app)
+        .post('/societies/removeCommitteeMember')
+        .set('Authorization', `Bearer ` + token)
+        .send({
+          societyId: 1,
+          userId: 1,
+        });
+    expect(res2.statusCode).toBe(400);
+    // Check the response
+    expect(res2.body).not.toBeNull();
+    expect(res2.body.message).toBe('Cannot remove president');
+    // Remove the user from the committee
+    await prisma.committee.delete({
+      where: {
+        userId_societyId: {
+          userId: 2,
+          societyId: 1,
+        },
+      },
+    });
   });
 });
 
@@ -203,5 +459,80 @@ describe('getCommitteeMember', () => {
     // Check the response
     expect(res.body).not.toBeNull();
     expect(res.body.message).toBe('Missing societyId');
+  });
+});
+
+describe('checkIfUserIsCommitteeMember', () => {
+  test('Check if user is committee member', async () => {
+    const res = await request(app)
+        .post('/societies/checkCommitteeMember')
+        .set('Authorization', `Bearer ` + token)
+        .send({
+          eventId: 1,
+        });
+    expect(res.statusCode).toBe(200);
+    // Check the response
+    expect(res.body).not.toBeNull();
+    expect(res.body.isCommitteeMember).toBe(true);
+  });
+
+  test('Check if user is committee member with no token', async () => {
+    const res = await request(app)
+        .post('/societies/checkCommitteeMember')
+        .send({
+          eventId: 1,
+        });
+    expect(res.statusCode).toBe(400);
+    // Check the response
+    expect(res.body).not.toBeNull();
+    expect(res.body.message).toBe('Unauthorized');
+  });
+
+  test('Send missing event ID', async () => {
+    const res = await request(app)
+        .post('/societies/checkCommitteeMember')
+        .set('Authorization', `Bearer ` + token);
+    expect(res.statusCode).toBe(400);
+    // Check the response
+    expect(res.body).not.toBeNull();
+    expect(res.body.message).toBe('Missing eventId');
+  });
+
+  test('Send invalid eventId', async () => {
+    const res = await request(app)
+        .post('/societies/checkCommitteeMember')
+        .set('Authorization', `Bearer ` + token)
+        .send({
+          eventId: 1000000,
+        });
+    expect(res.statusCode).toBe(404);
+    // Check the response
+    expect(res.body).not.toBeNull();
+    expect(res.body.message).toBe('Event not found');
+  });
+
+  test('Check if user is not committee member', async () => {
+    // Login as a user not in the committee
+    const res = await request(app)
+        .post('/user/login')
+        .send({
+          email: 'student@kcl.ac.uk',
+          password: 'student',
+        });
+    expect(res.statusCode).toBe(200);
+    // Check the response
+    expect(res.body).not.toBeNull();
+    expect(res.body.token).not.toBeNull();
+    const token = res.body.token;
+    const res2 = await request(app)
+        .post('/societies/checkCommitteeMember')
+        .set('Authorization', `Bearer ` + token)
+        .send({
+          eventId: 2,
+        });
+    expect(res2.statusCode).toBe(200);
+    // Check the response
+    expect(res2.body).not.toBeNull();
+    expect(res2.body.isCommitteeMember).toBe(false);
   });
 });
--- api/src/controllers/committee.js
+++ api/src/controllers/committee.js
@@ -69,7 +69,7 @@ async function addCommitteeMember(req, res) {
   });
 
   return res.status(200)
-      .send({message: 'User added to committee', userId: decoded.id});
+      .send({message: 'User added to committee', userId: user.id});
 }
 
 /**
@@ -86,51 +86,84 @@ async function removeCommitteeMember(req, res) {
   } catch (err) {
     return res.status(500).send({message: 'Internal Server Error'});
   }
+  // Check that the request body contains a userId and societyId
+  if (!req.body.userId || !req.body.societyId) {
+    return res.status(400).send({message: 'Invalid Request'});
+  }
+
+  // Check that the userId and the societyId are positive integers and are of
+  // type number
+  if (typeof req.body.userId !== 'number' ||
+    typeof req.body.societyId !== 'number' ||
+    req.body.userId < 0 ||
+    req.body.societyId < 0) {
+    return res.status(400).send({message: 'Invalid Request'});
+  }
+
+  // Check that user exists
+  const user = await prisma.user.findUnique({
+    where: {
+      id: req.body.userId,
+    },
+  });
+
+  if (user === null) {
+    return res.status(400).send({message: 'User does not exist'});
+  }
+
+  // Check that society exists
+  const society = await prisma.society.findUnique({
+    where: {
+      id: req.body.societyId,
+    },
+  });
+
+  if (society === null) {
+    return res.status(400).send({message: 'Society does not exist'});
+  }
+
   // Check if user is a committee member of the society
-  const committee = await prisma.committee.findMany({
+  const isCommittee = await prisma.committee.findFirst({
     where: {
       userId: decoded.id,
       societyId: req.body.societyId,
     },
-    select: {
+  });
+
+  if (!isCommittee) {
+    return res.status(400).send({message: 'Unauthorized'});
+  }
+
+  // Check if user is a committee member of the society
+  const isCommitteeMember = await prisma.committee.findFirst({
+    where: {
+      userId: req.body.userId,
+      societyId: req.body.societyId,
+    },
+  });
+
+  if (!isCommitteeMember) {
+    return res.status(400).send({message: 'User is not a committee member'});
+  }
+
+  // Check if user is president
+  const isPresident = await prisma.committee.findFirst({
+    where: {
+      userId: req.body.userId,
+      societyId: req.body.societyId,
       isPresident: true,
     },
   });
 
-  if (committee.length === 0) {
-    return res.status(401).send({message: 'Unauthorized'});
-  }
-
-  if (committee[0].isPresident === false) {
-    return res.status(400).send({message: 'User must be a President'});
-  }
-
-  // Check if the user is a committee member
-  const isCommitteeMember = await prisma.committee.findMany({
-    where: {
-      userId: req.body.userId,
-      societyId: req.body.societyId,
-      isPresident: false,
-    },
-  });
-
-  if (isCommitteeMember == 0) {
-    return res.status(400).send({message: 'User is not a committee member'});
-  }
-
-  console.log(isCommitteeMember);
-
-  if (isCommitteeMember[0].isPresident === true) {
-    return res.status(400).send({message: 'Cannot remove the president'});
+  if (isPresident) {
+    return res.status(400).send({message: 'Cannot remove president'});
   }
 
   // Remove the user from the committee
-  await prisma.committee.delete({
+  await prisma.committee.deleteMany({
     where: {
-      userId_societyId: {
-        userId: req.body.userId,
-        societyId: req.body.societyId,
-      },
+      userId: req.body.userId,
+      societyId: req.body.societyId,
     },
   });
 
@@ -171,45 +204,44 @@ async function getCommitteeMembers(req, res) {
  * @param {Response} res The response object
  */
 async function checkIfUserIsCommitteeMember(req, res) {
+  let decoded = null;
   try {
     // Authenticate the user
-    const userId = (await auth.authenticate(req)).id;
-
-    if (!req.body.eventId) {
-      res.status(400).send({message: 'Missing eventId'});
-      return;
-    }
-
-    const event = await prisma.event.findUnique({
-      where: {
-        id: req.body.eventId,
-      },
-      select: {
-        societyId: true,
-      },
-    });
-
-    if (!event) {
-      res.status(404).send({message: 'Event not found'});
-      return;
-    }
-
-    const committee = await prisma.committee.findMany({
-      where: {
-        userId: userId,
-        societyId: event.societyId,
-      },
-    });
-
-    if (committee.length === 0) {
-      res.status(200).send({isCommitteeMember: false});
-      return;
-    }
-
-    res.status(200).send({isCommitteeMember: true});
+    decoded = await auth.authenticate(req);
   } catch (err) {
-    res.status(500).send({message: 'Internal Server Error'});
+    return res.status(400).send({message: 'Unauthorized'});
   }
+
+  if (!req.body.eventId) {
+    res.status(400).send({message: 'Missing eventId'});
+    return;
+  }
+
+  const event = await prisma.event.findUnique({
+    where: {
+      id: req.body.eventId,
+    },
+    select: {
+      societyId: true,
+    },
+  });
+
+  if (!event) {
+    return res.status(404).send({message: 'Event not found'});
+  }
+
+  const committee = await prisma.committee.findMany({
+    where: {
+      userId: decoded.id,
+      societyId: event.societyId,
+    },
+  });
+
+  if (committee.length === 0) {
+    return res.status(200).send({isCommitteeMember: false});
+  }
+
+  return res.status(200).send({isCommitteeMember: true});
 }
 
 /**

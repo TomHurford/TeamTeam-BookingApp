df0c034ffe07a5d9c0b7a713cf8b78bffe11f2d5
AbdulAdal <k21007037@kcl.ac.uk>
2023-03-23 23:59:34 +0000

    Fix tests

--- client/cypress/e2e/Integration/Basket.spec.cy.js
+++ client/cypress/e2e/Integration/Basket.spec.cy.js
@@ -1,68 +1,67 @@
-describe('Basket', () => {
+describe("Basket", () => {
+  it("Simulate purchase with login", () => {
+    cy.visit("/login");
+    cy.get('input[name="loginEmail"]').type("admin@admin.com");
+    cy.get('input[name="loginPassword"]').type("admin123");
+    cy.get('button[name="loginbutton"]').click();
+    cy.wait(500);
+    cy.visit("/event-details?eventId=1");
 
-  it('Simulate purchase with login',() => {
-    cy.visit('/login')
-    cy.get('input[name="loginEmail"]').type('admin@admin.com')
-    cy.get('input[name="loginPassword"]').type('admin123')
-    cy.get('button[name="loginbutton"]').click()
-    cy.wait(500)
-    cy.visit('/event-details?eventId=1')
+    cy.wait(500);
+    cy.get("div[data-testid='right10']").click();
+    cy.get("div[data-testid='right10']").click();
+    cy.get("div[data-testid='right10']").click();
 
-    cy.wait(500)
-    cy.get("div[data-testid='right10']").click()
-    cy.get("div[data-testid='right10']").click()
-    cy.get("div[data-testid='right10']").click()
+    cy.wait(500);
+    cy.contains("Go To Basket").click();
 
-    cy.wait(500)
-    cy.contains('Go To Basket').click()
+    cy.wait(500);
+    cy.get("div[data-testid='left10']").click();
 
-    cy.wait(500)
-    cy.get("div[data-testid='left10']").click()
+    cy.contains("Checkout").click();
 
-    // cy.contains('Checkout').click()
+    cy.wait(500);
+    cy.url().should("include", "/tickets");
 
-    // cy.wait(500)
-    // cy.url().should('include', '/tickets')
-
-    cy.contains('Logout').click()
-  })
-
-  it('Simulate purchase without login',() => {
-    cy.visit('/event-details?eventId=1')
-
-    cy.wait(500)
-    cy.get("div[data-testid='right10']").click()
-    cy.get("div[data-testid='right10']").click()
-    cy.get("div[data-testid='right10']").click()
-
-    cy.wait(500)
-    cy.contains('Go To Basket').click()
-
-    cy.wait(500)
-    cy.contains('Log In').click()
+    cy.contains("Logout").click();
   });
 
-  it('Add tickets of a different event to basket',() => {
-    cy.visit('/event-details?eventId=1')
+  it("Simulate purchase without login", () => {
+    cy.visit("/event-details?eventId=1");
 
-    cy.wait(500)
-    cy.get("div[data-testid='right10']").click()
-    cy.get("div[data-testid='right10']").click()
-    cy.get("div[data-testid='right10']").click()
+    cy.wait(500);
+    cy.get("div[data-testid='right10']").click();
+    cy.get("div[data-testid='right10']").click();
+    cy.get("div[data-testid='right10']").click();
 
-    cy.wait(500)
+    cy.wait(500);
+    cy.contains("Go To Basket").click();
 
-    cy.visit('/event-details?eventId=3')
-    cy.get("div[data-testid='right10']").click()
-    cy.get("div[data-testid='right10']").click()
-    cy.get("div[data-testid='right10']").click()
-  })
+    cy.wait(500);
+    cy.contains("Log In").click();
+  });
 
-  it('Click remove ticket button on empty basket',() => {
-    cy.visit('/event-details?eventId=1')
-    cy.wait(500)
+  it("Add tickets of a different event to basket", () => {
+    cy.visit("/event-details?eventId=1");
 
-    cy.get("div[data-testid='left10']").click()
-    cy.get("div[data-testid='left10']").click()
-  })
-})
+    cy.wait(500);
+    cy.get("div[data-testid='right10']").click();
+    cy.get("div[data-testid='right10']").click();
+    cy.get("div[data-testid='right10']").click();
+
+    cy.wait(500);
+
+    cy.visit("/event-details?eventId=3");
+    cy.get("div[data-testid='right10']").click();
+    cy.get("div[data-testid='right10']").click();
+    cy.get("div[data-testid='right10']").click();
+  });
+
+  it("Click remove ticket button on empty basket", () => {
+    cy.visit("/event-details?eventId=1");
+    cy.wait(500);
+
+    cy.get("div[data-testid='left10']").click();
+    cy.get("div[data-testid='left10']").click();
+  });
+});
